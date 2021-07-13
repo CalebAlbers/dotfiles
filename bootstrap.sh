5114d75a8a23af18c7147277a8ed30a5198b0220
Caleb Albers <calebalbers@gmail.com>
2021-07-13 13:59:27 -0700

    remove settle

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,17 +10,12 @@ brew bundle install
 # update all mac apps
 brew cu
 
-npm install -g n # node version manager
-npm install -g yo
-npm install -g sao
-npm install -g nodemon
-npm install -g typescript
-npm install -g npm-merge-driver
+source ./scripts/jstools.sh
 
 # hide icons from desktop
 source ./scripts/defaults.sh
 
-
-
-
 # set default app to vs code for most extensions???
+
+# set Chrome as default
+open -a "Google Chrome" --args --make-default-browser
