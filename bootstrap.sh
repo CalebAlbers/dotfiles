5ffdc1587bd925c185ffa3e26ce2ed9165156bd9
Caleb Albers <calebalbers@gmail.com>
2021-07-13 21:37:39 -0700

    Finish structure for zero

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,21 @@
 # install xcode
-# `xcode-select --install`
+if ! xcode-select -p 1> /dev/null
+then
+  echo "Installing XCode Command Line Tools..."
+  xcode-select --install
+fi
 
 # install homebrew
 if ! command -v brew &> /dev/null
 then
-  echo "Installing brew..."
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install Zero
-$ brew install zero-sh/tap/zero
+brew install zero-sh/tap/zero
 
 # Initialize Setup
 zero setup
 
-# configure brew to auto-update
+# configure homebrew to auto-update
